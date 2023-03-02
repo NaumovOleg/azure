38927d241a5dcf2dbff53bec0c7187d1e9e388b7
Oleg <keeperoleg26@gmail.com>
2023-03-02 17:30:31 +0200

    routes

--- User/index.ts
+++ User/index.ts
@@ -1,5 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
+
 const httpTrigger: AzureFunction = async function (context: Context, _: HttpRequest): Promise<void> {
   context.log('HTTP trigger function processed a request.');
 
