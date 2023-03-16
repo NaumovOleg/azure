3b220309e8e8a4922998bf13bf55e72d0260bff9
Oleg <keeperoleg26@gmail.com>
2023-03-16 11:55:52 +0200

    CI

--- User/index.ts
+++ User/index.ts
@@ -1,9 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { config } from  'dotenv';
 import connection  from '../shared/dal/connection';
 import { User } from '../shared/dal/models';
 
+config();
+
 const httpTrigger: AzureFunction = async function (context: Context, _: HttpRequest): Promise<void> {
+    
+  console.log('ddddddddddddddddddddddddddddddddddaaaaaaddddd',process.env); 
   await connection.open();
+
 
   const user  = new User({ name: 'tes' });
 
